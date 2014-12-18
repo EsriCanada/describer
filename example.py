5e2ebf49d8ced88234255108da6d0fab7219b4ee
Adam <amarinelli@esri.ca>
2014-12-18 16:33:32 -0500

    Updated Example.py

--- example.py
+++ example.py
@@ -6,20 +6,19 @@ D = describer.Desc()
 
 
 
-# Usage-1
-# Will return list with all valid properties
+# Usage 1
+# Will return python list with all valid properties
 
 properties = D.find_properties(desc)
+
+print properties
 
 for prop in properties:
     print(prop)
 
 
-print("\n|----------------------|\n")
 
-
-
-# Usage-2
-# Will print property and returned value
+# Usage 2
+# Will print all available properties and their returned value(s)
 
 D.pretty_properties(desc)
